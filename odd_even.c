563edd56b585187bcf9c71c8388cc9dc2eca2762
Ranveer Singh <ranveers3795@gmail.com>
2026-07-14 16:55:23 +0530

    odd_even.c

--- odd_even.c
+++ odd_even.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main() {
-    t num;
+    int num;
     printf("Enter a number: ");
-    scanf("%", num);
+    scanf("%d", &num);
 
-    if (num % 2 == 0) {
+    if (num % 2 == 0) {// Check if the number is divisible by 2
         printf("%d is even.\n", num);
     } else {
         printf("%d is odd.\n", num);
